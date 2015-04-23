933250cd9cbd6038372cf930da2aec9ab10a12db
Amy Ton <amyhton@gmail.com>
2015-04-23 12:30:46 -0500

    add revealscroll elements

--- js/main.js
+++ js/main.js
@@ -4,9 +4,15 @@ jQuery(document).ready(function($){
 		if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
 	});
 
-  // calling the plugin
+  // calling the parallax plugin
   $('#bg-1').parallax('50%', 0.3);
   $('#bg-2').parallax('50%', 0.3);
   $('#bg-3').parallax('50%', 0.3);
+
+  // if you want to keep seeing the animation on scroll
+  // var config = { reset: true };
+  // window.sr = new scrollReveal(config);
+
+  window.sr = new scrollReveal();
 });
 
